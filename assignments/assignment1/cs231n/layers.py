2f6c36a4bb6ebf967634c4b6e197501c3a9e7e93
WenboKou <kouwenbocn@163.com>
2024-07-04 13:55:53 +0000

    affine layer backward

--- assignments/assignment1/cs231n/layers.py
+++ assignments/assignment1/cs231n/layers.py
@@ -28,8 +28,8 @@ def affine_forward(x, w, b):
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    x = x.reshape(x.shape[0], -1)
-    out = x @ w + b
+    x_reshape = x.reshape(x.shape[0], -1)
+    out = x_reshape @ w + b
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
     #                             END OF YOUR CODE                            #
@@ -60,8 +60,10 @@ def affine_backward(dout, cache):
     # TODO: Implement the affine backward pass.                               #
     ###########################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    x_reshape = x.reshape(x.shape[0], -1)
+    dx = (dout @ w.T).reshape(x.shape)
+    dw = x_reshape.T @ dout
+    db = dout.sum(axis=0)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     ###########################################################################
